67837f4d76b1f88ff0a15409f1ebab8434c4ce67
Lucas Pereira <luquinhas.lp35@hotmail.com>
2024-05-21 14:01:32 -0300

    finalizado

--- dia 4/jantar-especial/index.js
+++ dia 4/jantar-especial/index.js
@@ -5,7 +5,7 @@ const restauranteAberto = prompt("O restaurante está aberto? (sim/nao)")
 if (temFome === "não" || temDinehiro === "não") {
     console.log("Hoje a janta será em casa");
 } else if (temDinehiro === "sim" && restauranteAberto === "sim") {
-    console.log("Hoje a janta será no seu restaurante favorito");
+    console.log("Hoje a janta será no seu restaurante favorito.");
 } else {
     console.log ("Peça um delivery!");
 }    
