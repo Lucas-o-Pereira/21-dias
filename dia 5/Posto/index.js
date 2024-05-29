8384779a683894707eef9808578d27da3dd6119a
Lucas Pereira <luquinhas.lp35@hotmail.com>
2024-05-29 20:12:18 -0300

    Posto

---
+++ dia 5/Posto/index.js
@@ -0,0 +1,21 @@
+// Criando variáveis
+let valor
+let quantidade
+let opção = prompt("Bem vindo ao nosso posto de gasolina!, você deseja: " + "\n1 - abastecer com gasolina; \n2 - abastecer com álcool; \n3 - calibrar os pneus")
+
+// Criando switchcase
+switch (opção){
+    case "1": 
+    valor parseInt(prompt("Digite o valor desejado para abastecer: "))
+    quantidade = valor / 5
+    console.log("foram abastecidos " + quantidade + "L de gasolina")
+    break;
+    case "2":
+        valor = parseInt(prompt("Digite o valor desejado para abastecer: "))
+        quantidade = valor / 3
+        console.log("Foram abastecidos " + quantidade + "L de álcool") 
+        break;
+        default:
+            console.log("Pneeus calibrados com sucesso")
+            break;
+}
