3d19bf9e69a29be6b3f65b6ac9bc38fc636e054d
Lucas Pereira <luquinhas.lp35@hotmail.com>
2024-05-28 23:28:48 -0300

    num

--- dia 5/calculadora/index.js
+++ dia 5/calculadora/index.js
@@ -15,6 +15,6 @@ switch  (opção) {
         console.log(num1 + "*" + num2 + "=" + (num1 * num2))
         break;
         default:
-            console.log(nuum1 + "/" + num2 + "=" + (num1 / num2))
+            console.log(num1 + "/" + num2 + "=" + (num1 / num2))
             break:
 }
